7607809868e26e227ac806143b75e061757d29df
WasiullaGit <106450412+WasiullaGit@users.noreply.github.com>
2026-04-18 14:42:10 +0530

    commonds corrected

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-run: sudo apt-get install cowsay -y
-run: cowsay -f dragon "Run for cover, I am a DRAGON.....RAWS" >> dragon.txt
-run: grep -i "dragon" dragon.txt
-run: cat dragon.txt
-run: ls           
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover, I am a DRAGON.....RAWS" >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls           
